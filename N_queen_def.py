dc161e2331db50a1580b051b1ff5da278e5b315d
Andrii Cherpak <andriycherpak.education@gmail.com>
2021-02-14 18:24:58 +0200

    renamed some variables, task added

--- N_queen_def.py
+++ N_queen_def.py
@@ -1,53 +1,53 @@
-def search(p, d, q, size):
-    for i in range(len(p)):
-        n_pos = p.copy()
-        c_q = q
-        c_desk = [d[i].copy() for i in range(size)]
-        c_q = fill(p[i][0], p[i][1], c_q, n_pos, c_desk, size)
+def search(positions, desk, num_of_queens, size):
+    for i in range(len(positions)):
+        n_pos = positions.copy()
+        c_q = num_of_queens
+        copy_desk = [desk[i].copy() for i in range(size)]
+        c_q = fill(n_pos[i][0], n_pos[i][1], c_q, n_pos, copy_desk, size)
         if c_q == size:
             string = ''
-            for l in c_desk:
-                for e in l: string += e
+            for line in copy_desk:
+                for symbol in line: string += symbol
                 string += '\n'
             return string[:-1]
-        elif len(n_pos)>0:
-            s = search(n_pos, c_desk, c_q, size)
-            if s!=None: return s
+        elif len(n_pos) > 0:
+            s = search(n_pos, copy_desk, c_q, size)
+            if s: return s
 
-def fill(r, c, q, pos, desk, size):
-    q += 1
-    desk[r][c] = "Q"
-    pos.remove((r, c))
+def fill(row, column, num_of_queens, positions, desk, size):
+    num_of_queens += 1
+    desk[row][column] = "Q"
+    positions.remove((row, column))
     for j in range(size):
-        if desk[r][j] == 0:
-            desk[r][j] = '.'
-            pos.remove((r, j))
+        if desk[row][j] == 0:
+            desk[row][j] = '.'
+            positions.remove((row, j))
     for i in range(size):
-        if desk[i][c] == 0:
-            desk[i][c] = '.'
-            pos.remove((i, c))
-    if r>=c:
-        n = r-c
+        if desk[i][column] == 0:
+            desk[i][column] = '.'
+            positions.remove((i, column))
+    if row>=column:
+        n = row-column
         m = 0
     else:
         n = 0
-        m = c-r
+        m = column-row
     while n < size and m < size:
         if desk[n][m] == 0:
             desk[n][m] = '.'
-            pos.remove((n, m))
+            positions.remove((n, m))
         n += 1
         m += 1
-    if r+c<=size-1:
+    if row+column<=size-1:
         n = 0
-        m = r+c
+        m = row+column
     else:
-        n = r+c-size+1
+        n = row+column-size+1
         m = size-1
     while n < size and m >= 0:
         if desk[n][m] == 0:
             desk[n][m] = '.'
-            pos.remove((n, m))
+            positions.remove((n, m))
         n += 1
         m -= 1
-    return q
+    return num_of_queens
--- N_queen_main.py
+++ N_queen_main.py
@@ -1,10 +1,13 @@
 from N_queen_def import *
 
-def chess(size, r, c):
-    q = 0
-    desk = [[0] * size for i in range(size)]
-    pos = [(a, b) for a in range(size) for b in range(size)]
 
-    q = fill(r, c, q, pos, desk, size)
-    s = search(pos, desk, q, size)
-    return ("" if s == None else s)
+def chess(size, row, column):
+    num_of_queens = 0
+    desk = [[0] * size for i in range(size)]
+    positions = [(a, b) for a in range(size) for b in range(size)]
+
+    num_of_queens = fill(row, column, num_of_queens, positions, desk, size)
+    res = search(positions, desk, num_of_queens, size)
+    return res if res else ""
+
+print(chess(5, 0, 2))
