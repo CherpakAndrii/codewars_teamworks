cfdf3ef3e4483243e52f39506f4c7da7b11c39d6
Andrii Cherpak <andriycherpak.education@gmail.com>
2021-02-16 22:49:02 +0200

    starting translation from python to cpp

---
+++ N_Queen_Problem/N_Queen_Problem.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+char** chess(int size, int row, int column) {
+    vector<vector<int>> pos;
+    int num_of_queens = 0;
+    char** desk = new char* [size];
+    for (int i = 0; i < size; i++){
+        desk[i] = new char[size];
+        for (int j = 0; j < size; j++) {
+            pos.push_back({ i, j });
+        }
+    }
+    
+}
+
+int fill(int row, int column, int num_of_queens, vector<vector<int>>& pos, char**& desk, int size) {
+    num_of_queens += 1;
+    desk[row][column] = 'Q';
+    int i = 0;
+    for (vector<int> l:pos) {
+        if (pos[i][0] == row || pos[i][1] == column) {
+            pos.erase(pos.begin() + i, pos.begin() + i+1);
+        }
+        else i++;
+    }
+
+}
